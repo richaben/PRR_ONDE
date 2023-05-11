6240b2d07db585e77e77002ffa0662b8622cf45a
broussel59 <106540903+broussel59@users.noreply.github.com>
2023-05-11 15:03:00 +0200

    Adaptation du paramétrage à la région

--- _config.R
+++ _config.R
@@ -1,4 +1,4 @@
 ## configuration
-conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Île-de-France"
-conf_auteur <- "OFB - Direction régionale Île-de-France"
-conf_dep <- c("75", "77", "78", "91", "92", "93", "94", "95")
+conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Hauts-de-France"
+conf_auteur <- "OFB - Direction régionale Hauts de France"
+conf_dep <- c("59", "60", "62", "02", "80")
