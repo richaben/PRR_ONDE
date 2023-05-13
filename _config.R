c27d65b150b8db9c83827bb212eaa32916b112f8
Office français de la biodiversité - Normandie <132992949+OFB-Nor@users.noreply.github.com>
2023-05-13 22:58:49 +0200

    Back in Normandie

--- _config.R
+++ _config.R
@@ -1,5 +1,4 @@
 ## configuration
 conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Normandie"
 conf_auteur <- "OFB - Direction régionale Normandie"
-#conf_dep <- c("14", "27", "50", "61", "76")
-conf_dep <- c("16", "17", "19", "23", "24", "33", "40", "47", "64", "79", "86", "87")
+conf_dep <- c("14", "27", "50", "61", "76")
