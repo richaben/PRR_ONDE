3b84047954d58e853bd112d2309bd11972494653
cedric.mondy <cedric.mondy@ofb.gouv.fr>
2023-05-25 09:20:42 +0200

    Complète la vérification de besoin de mise-à-jour

--- 01_verifier_maj.R
+++ 01_verifier_maj.R
@@ -25,37 +25,47 @@ if (!file.exists("data/onde_data/onde.csv") |
       dplyr::distinct(code_departement, date_campagne) %>% 
       dplyr::rename(old = date_campagne)
   
-  to_update <- dplyr::left_join(
-    x = old_data,
-    y = purrr::map_df(
-      .x = unique(old_data$code_departement),
-      function(dep) {
-        hubeau::get_ecoulement_campagnes(
+  new_data <- purrr::map_df(
+    .x = conf_dep,
+    function(dep) {
+      hubeau::get_ecoulement_campagnes(
         code_departement = dep,
         fields = "code_departement,date_campagne"
-        )
-        }
-      ) %>% 
-      dplyr::mutate(
-        date_campagne = lubridate::as_date(date_campagne, format = "%Y-%m-%d")
-        ) %>% 
-      dplyr::group_by(code_departement) %>% 
-      dplyr::filter(date_campagne == max(date_campagne)) %>% 
-      dplyr::ungroup() %>% 
-      dplyr::rename(new = date_campagne),
-    by = "code_departement"
+      )
+    }
   ) %>% 
-    dplyr::mutate(update = new > old) %>% 
-    dplyr::pull(update) %>% 
-    (function(x) {
-      if (any(x)) {
-        TRUE
-      } else {
-        FALSE
-      }
-    })
+    dplyr::mutate(
+      date_campagne = lubridate::as_date(date_campagne, format = "%Y-%m-%d")
+    ) %>% 
+    dplyr::group_by(code_departement) %>% 
+    dplyr::filter(date_campagne == max(date_campagne)) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::rename(new = date_campagne)
+  
+  if (
+    any(! old_data$code_departement %in% new_data$code_departement) |
+    any(! new_data$code_departement %in% old_data$code_departement)
+    ) {
+    to_update <- TRUE
+  } else {
+      to_update <- dplyr::left_join(
+        x = old_data,
+        y = new_data,
+        by = "code_departement"
+        ) %>% 
+        dplyr::mutate(update = new > old) %>% 
+        dplyr::pull(update) %>% 
+        (function(x) {
+          if (any(x)) {
+            TRUE
+            } else {
+              FALSE
+              }
+          })
+  }
 }
 
 save(to_update, date_jour, date_jour_heure, file = "data/onde_data/to_update.rda")
   
+print(switch(as.character(to_update), `TRUE` = "Mise-à-jour requise", `FALSE` = "Pas de mise-à-jour requise"))
 
