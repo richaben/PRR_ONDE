67c9a1e5c5d9fcd309a35f14e8c0842de49a5fab
Office français de la biodiversité - Île-de-France <128606748+OFB-IdF@users.noreply.github.com>
2023-05-30 13:34:15 +0200

    Update _config.R

--- _config.R
+++ _config.R
@@ -1,4 +1,4 @@
 ## configuration
 conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Île-de-France"
 conf_auteur <- "OFB - Direction régionale Île-de-France"
-conf_dep <- c("75", "77", "78", "91", "92", "93", "94", "95)
+conf_dep <- c("75", "77", "78", "91", "92", "93", "94", "95")
